63b9aa00637caac661f4ec16cd23d10997881183
Zubair Qazi <zqazi004@ucr.edu>
2020-01-08 23:11:53 -0800

    Partially implemented registration

--- app/src/main/java/com/zubairqazi/coffeetracker/Signup.kt
+++ app/src/main/java/com/zubairqazi/coffeetracker/Signup.kt
@@ -1,14 +1,14 @@
 package com.zubairqazi.coffeetracker
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -17,8 +17,11 @@ class Signup : AppCompatActivity() {
     val TAG = "SignupActivity"
     val EXTRA_MESSAGE = "com.zubairqazi.coffeetracker.MESSAGE"
 
-    lateinit var editUserEmail: EditText
-    lateinit var editUserPwd: EditText
+    private lateinit var editUserEmail: EditText
+    private lateinit var editUserPwd: EditText
+
+    private lateinit var btnRegister: Button
+    private lateinit var btnLogin: Button
 
     val db = FirebaseFirestore.getInstance()
     private lateinit var auth: FirebaseAuth
@@ -30,15 +33,15 @@ class Signup : AppCompatActivity() {
         editUserEmail = findViewById(R.id.emailField)
         editUserPwd = findViewById(R.id.pwdField)
 
-        val btnRegister = findViewById<Button>(R.id.buttonRegister)
-        val btnLogin = findViewById<Button>(R.id.buttonLogin)
+        btnRegister = findViewById(R.id.buttonRegister)
+        btnLogin = findViewById(R.id.buttonLogin)
 
         btnRegister.setOnClickListener {
             registerUser()
         }
 
         btnLogin.setOnClickListener {
-
+            TODO("Login not yet implemented.")
         }
 
         auth = FirebaseAuth.getInstance()
@@ -49,12 +52,31 @@ class Signup : AppCompatActivity() {
         val userPwd: String? = editUserPwd.text.toString()
 
         if (userEmail.isNullOrBlank()) {
-            TODO("Email field check not yet implemented.")
+            Toast.makeText(this, "Please enter an email.", Toast.LENGTH_SHORT).show()
+            // Add a toast message
+            return
         }
         if (userPwd.isNullOrBlank()) {
-            TODO("Password field check not yet implemented.")
+            Toast.makeText(this, "Please enter a password.", Toast.LENGTH_SHORT).show()
+            // Add a toast message
+            return
         }
 
+        auth.createUserWithEmailAndPassword(userEmail, userPwd)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+//                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+            }
 
     }
 
